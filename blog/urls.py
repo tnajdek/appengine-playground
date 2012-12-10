e25f7fd5f6580e6e6c942774a5a218602743f44b
Tom Najdek <tom@doppnet.com>
2012-12-10 01:08:16 +0000

    Add some security.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
-from django.views.generic import ListView, DetailView, RedirectView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView
 from models import Post
 from forms import PostForm
 from views import CreateView, UpdateView, DeleteView, admin_home
-from django.contrib.auth.decorators import login_require
+from django.contrib.auth.decorators import login_required
 
 # Django 1.3 doesn't get to be lazy!
 try:
@@ -17,24 +17,24 @@ except ImportError:
 urlpatterns = patterns('',
 	# Somehow I don't like django contrib admin
 	# Pluse it feels bit like cheating, so..
-	url(r'^admin/post/$', ListView.as_view(
+	url(r'^admin/post/$', login_required(ListView.as_view(
 		paginate_by=10,
 		model=Post
-		), name="post-list"
+		)), name="post-list"
 	),
-	url(r'^admin/post/createform/$', CreateView.as_view(
+	url(r'^admin/post/createform/$', login_required(CreateView.as_view(
 		model=Post,
 		form_class=PostForm,
-		), name="post-createform"
+		)), name="post-createform"
 	),
-	url(r'^admin/post/(?P<pk>\d+)/editform/$', UpdateView.as_view(
+	url(r'^admin/post/(?P<pk>\d+)/editform/$', login_required(UpdateView.as_view(
 		model=Post,
 		form_class=PostForm,
-		), name="post-editform"
+		)), name="post-editform"
 	),
-	url(r'^admin/post/(?P<pk>\d+)/deleteform/$', DeleteView.as_view(
+	url(r'^admin/post/(?P<pk>\d+)/deleteform/$', login_required(DeleteView.as_view(
 		model=Post
-		), name="post-deleteform"
+		)), name="post-deleteform"
 	),
 	url(r'^admin/$', admin_home, name="admin"),
 	url(r'^$', ListView.as_view(
